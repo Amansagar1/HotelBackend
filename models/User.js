05fa7b6891c22da6a7e533b4949b653ff42f5027
sagar sharma <sagarsharmaofficial0@gmail.com>
2024-12-13 00:37:49 +0530

    login added

--- models/User.js
+++ models/User.js
@@ -1,26 +1,58 @@
 
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// const userSchema = new Schema({
+//   username: {
+//     type: String,
+//     required: [true, 'Username is required'], // Ensure 'username' is required
+//     unique: true,
+//   },
+//   email: {
+//     type: String,
+//     required: [true, 'Email is required'],
+//     unique: true,
+//   },
+//   password: {
+//     type: String,
+//     required: [true, 'Password is required'],
+//   },
+//   role: {
+//     type: String,
+//     default: 'user', // Example role
+//   },
+// });
+
+// module.exports = mongoose.model('User', userSchema);
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   username: {
     type: String,
-    required: [true, 'Username is required'], // Ensure 'username' is required
-    unique: true,
+    required: [true, 'Full Name is required'],
   },
   email: {
     type: String,
     required: [true, 'Email is required'],
     unique: true,
   },
+  mobileNumber: {
+    type: String,
+    required: [true, 'Mobile Number is required'],
+  },
   password: {
     type: String,
     required: [true, 'Password is required'],
   },
-  role: {
+  confirmPassword: {
     type: String,
-    default: 'user', // Example role
+    required: [true, 'Password is required'],
   },
-});
+  // role: {
+  //   type: String,
+  //   default: 'user', // Default role is 'user'
+  // },
+}, );
 
 module.exports = mongoose.model('User', userSchema);
