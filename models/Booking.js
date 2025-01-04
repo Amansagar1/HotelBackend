4508b89b9acd8f7757562631f09fefbd30ffc332
sagar sharma <sagarsharmaofficial0@gmail.com>
2025-01-04 10:17:39 +0530

    booking api correct

--- models/Booking.js
+++ models/Booking.js
@@ -6,19 +6,44 @@ const bookingSchema = new mongoose.Schema({
   address: { type: String, required: true },
   city: { type: String, required: true },
   pincode: { type: String, required: true },
-  phone: { type: String, required: true },
-  email: { type: String, required: true },
+  phone: { 
+    type: String, 
+    required: true, 
+    match: [/^\d{10}$/, 'Please enter a valid 10-digit phone number.'] 
+  },
+  email: { 
+    type: String, 
+    required: true, 
+    match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address.'] 
+  },
   checkIn: { type: Date, required: true },
   checkOut: { type: Date, required: true },
-  checkInTime: { type: String, required: true },
-  checkOutTime: { type: String, required: true },
+  checkInTime: { 
+    type: String, 
+    required: true, 
+    match: [/^([01]?[0-9]|2[0-3]):([0-5][0-9])$/, 'Please enter a valid time in HH:MM format.']
+  },
+  checkOutTime: { 
+    type: String, 
+    required: true, 
+    match: [/^([01]?[0-9]|2[0-3]):([0-5][0-9])$/, 'Please enter a valid time in HH:MM format.']
+  },
   roomPreference: { type: String },
   numberOfAdults: { type: Number, required: true },
   roomId: { type: String, required: true },
   price: { type: Number, required: true },
   title: { type: String, required: true },
   bookingDate: { type: Date, default: Date.now },
-  available : { type: String, required: true},
+  available: { 
+    type: String, 
+    required: true,
+    
+  },
+  payment: { 
+    type: String, 
+    required: true, 
+    enum: ['paid', 'unpaid'], // Assuming you want payment status to be either paid or unpaid.
+  },
 });
 
 module.exports = mongoose.model("Booking", bookingSchema);
