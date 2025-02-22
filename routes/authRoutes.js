fe9f052b91b5e029086334c0b21fe3357ec5a7ac
sagar sharma <sagarsharmaofficial0@gmail.com>
2025-02-22 22:17:01 +0530

    backend updated

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,10 +10,9 @@ router.post('/auth/login', login);
 router.get('/users', authenticate,getAllUsers);
 
 
-
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/auth/google/callback', passport.authenticate('google', { session: false }), googleCallback);
 
 
 module.exports = router;
 // // Google OAuth Routes
-// router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-// router.get('/auth/google/callback', passport.authenticate('google', { session: false }), googleCallback);
